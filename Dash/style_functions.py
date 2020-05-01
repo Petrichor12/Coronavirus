d591975a6c00a7b7e124c7f546a69b8392388c1d
Petrichor12 <jason.collis0@gmail.com>
2020-05-01 21:13:13 +0200

    updated files

--- Dash/style_functions.py
+++ Dash/style_functions.py
@@ -14,4 +14,17 @@ def generate_radio_button(list, id):
                     }
     )
 
+def generate_slider(days, id):
+    days_slider = list(range(20, max(days), 5))
+    days_slider.sort()
+    min_days = days_slider[0]
+    max_days = days_slider[len(days_slider) - 1]
 
+    return dcc.Slider(
+         id=id,
+         min = min_days,
+         max = max_days,
+         value = 115,
+         marks = {str(date): str(date) for date in days_slider},
+         step = None
+    )
